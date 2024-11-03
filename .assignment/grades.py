cce94ab0dcec326f87bc6c5e3d411b29edf3bdd4
Caiyi Shyu <cai1hsu@outlook.com>
2024-11-03 17:12:50 +0800

    修复不含main函数的库测试失败

--- .assignment/grades.py
+++ .assignment/grades.py
@@ -37,7 +37,7 @@ def get_exercise_src_file(exercise_name):
     return None
 
 def prepare_format_check(exercise_path):
-    destination = "format_check/src/main.rs"
+    destination = "format_check/src/lib.rs"
     shutil.copyfile(exercise_path, destination)
 
 def cargo_fmt_check(manifest_path):
